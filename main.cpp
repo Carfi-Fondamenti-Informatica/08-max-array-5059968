6dae5529a1daeda7cd07e9fd71eb90d250c378b4
5059968 <128074987+5059968@users.noreply.github.com>
2023-05-26 10:20:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-
-int main(){
-  return 0;
+#include "libb.h"
+using namespace std;
+int main() {
+    int dim=0;
+    cin>>dim;
+    float num[dim];
+    for (int i=0;i<dim;i++){
+       cin>>num[i];
+    }
+    cout<<massimo(num,dim)<<endl;
+    return 0;
 }

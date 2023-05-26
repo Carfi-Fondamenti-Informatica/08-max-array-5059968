a97fb00608aea4a774d7f23e5a520dbdb6eafae2
5059968 <128074987+5059968@users.noreply.github.com>
2023-05-26 10:21:06 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
 
+#include "libb.h"
+float massimo(float num[],int dim){
+    float max;
+        for (int i = 0; i < dim; i++) {
+        if(num[i]>max){
+            max=num[i];
+        }
+        }
+
+    return max;
+}
